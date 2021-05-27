f431e0ba7035eabef09d3393da350782787679d5
Formacion <formacion@FTAULAS.ORG>
2021-05-27 12:36:08 +0200

    oreden operativo

---
+++ main.js
@@ -0,0 +1,15 @@
+let resultado;
+
+resultado = 2+5 *4;
+console.log(resultado);
+ 
+resultado = (2+5)*4 ;
+console.log(resultado);
+
+resultado = (20+30+30+50)*0.2;
+console.log(resultado);
+
+
+resultado = (2*5)*(3+4);
+console.log(resultado);
+
